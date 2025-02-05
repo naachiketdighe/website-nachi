8d18ce27bb7c3cb04f3357e398bb7f74f97f831c
Nachiket Dighe <dighent@mail.uc.edu>
2025-02-05 20:21:34 +0100

    updated mobile Navbar

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,6 +1,11 @@
+import { Bars3Icon } from '@heroicons/react/16/solid';
 import react from 'react';
 
-const Nav = () => {
+interface Props {
+    openNav: () => void;
+}
+
+const Nav = ({openNav}:Props) => {
     return ( 
     <div className="w-[100%] top-0 h-[12vh] bg-green-700 shadow-md"> 
         <div className='flex items-center justify-between w-[80%] mx-auto h-[100%]'>
@@ -11,7 +16,7 @@ const Nav = () => {
             <div className="nav-link">Leadership Experience</div>
             <div className="nav-link">Highlights</div>
             <div className="nav-link">Contact Me</div>
-
+            <div onClick={openNav}> <Bars3Icon className="w-[2rem] md:hidden h-[2rem] cursor-pointer text-white"></Bars3Icon></div>
 
             </div>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState} from "react";
 import Nav from "../components/Nav";
+import MobileNav from "../components/MobileNav";
 
 const Homepage = () => {
-  return <div className="overflow-x-hidden">
-    <Nav></Nav>
-  </div>;
+
+  const [nav,setNav] =  useState(false);
+  const openNav = () => setNav(true);
+  const closeNav = () => setNav(false);
+
+  return (
+  <div className="overflow-x-hidden">
+      <div>
+        <MobileNav nav={nav} closeNav={closeNav} /> 
+        <Nav openNav={openNav} />
+      </div>
+    
+  </div>
+  );
 };
 
 export default Homepage;
